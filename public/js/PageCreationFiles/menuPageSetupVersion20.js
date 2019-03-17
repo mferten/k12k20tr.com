0f551f6a45fafc546c7bcfb763cdfe0c4fa0f818
Mehmet Erten <mferten@yahoo.com>
2019-03-16 21:14:30 -0400

    commit all

--- public/js/PageCreationFiles/menuPageSetupVersion20.js
+++ public/js/PageCreationFiles/menuPageSetupVersion20.js
@@ -55,6 +55,11 @@ function setMenuImage(firstTime) {
     menuFooter.appendChild(menuFooterP);
     menuFooter.appendChild(menuFooterP2);
 
+    var menuImageCaption = document.createElement("figcaption");
+    menuImageCaption.setAttribute("class","borderImage");
+    menuImageCaption.innerHTML = "Doğal bir Çiftliği, Web şirketli bir Han olarak yaratmak";
+    menuMain.appendChild(menuImageCaption);
+
     var menuImage = document.createElement("img");
     menuImage.setAttribute("src","/images/_world.png");
     menuImage.setAttribute("alt","World View");
